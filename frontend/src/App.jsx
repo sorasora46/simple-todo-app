f57ddcebfc3fd1e1abf02dcbfebdc0ab59aa395f
sora-k46 <sorrawit456789@gmail.com>
2022-12-22 01:15:47 +0700

    [+] styling input form and button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from "react";
 import "./style.css";
 
 function App() {
-  const [task, setTask] = useState()
+  const [task, setTask] = useState();
 
   function handleSubmit(event) {
-    alert("Task submitted")
-    event.preventDefault()
-    event.stopPropagation()
+    alert("Task submitted");
+    event.preventDefault();
+    event.stopPropagation();
   }
 
   function handleChange(event) {
-    setTask(event.target.value)
+    setTask(event.target.value);
   }
 
   return (
@@ -19,12 +19,24 @@ function App() {
       <div className="w-[50%] h-[50vh] border rounded-xl py-10 px-5 shadow-lg transition ease-in-out delay-150 hover:scale-105 hover:shadow-2xl">
         <div className="flex flex-col justify-center items-center">
           <h1 className="font-bold text-2xl text-center">To Do App</h1>
-          <div>
+          <div className="w-[100%] flex justify-center gap-8 my-6">
             <form onSubmit={(event) => handleSubmit(event)}>
-              <input required onChange={(event) => handleChange(event)} value={task} type="text" placeholder="Enter a task here" />
-              <button type="submit">SAVE</button>
+              <input
+                required
+                onChange={(event) => handleChange(event)}
+                value={task}
+                type="text"
+                placeholder="Enter a task here"
+                className="mr-8 border rounded focus:outline-none focus:ring focus:border-blue-500 focus:rounded px-6 py-2"
+              />
+              <button
+                type="submit"
+                className="rounded text-white bg-blue-600 px-6 py-2 shadow-md shadow-blue-600/50"
+              >
+                SAVE
+              </button>
             </form>
-            <button>GET TASKS</button>
+            <button className="rounded text-white bg-yellow-500 px-6 py-2 shadow-md shadow-yellow-500/50">GET TASKS</button>
           </div>
         </div>
       </div>
