6dbf644e3ee5f93e3e55af859384d33a3277c344
sora-k46 <sorrawit456789@gmail.com>
2022-12-22 00:35:14 +0700

    [+] styling container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import "./style.css"
+import "./style.css";
 
 function App() {
   return (
     <div className="h-[100vh] flex justify-center items-center">
-      <div className="w-[50%] h-[50vh] border rounded-xl py-10 px-5">
-        <h1 className="font-bold">To Do App</h1>
+      <div className="w-[50%] h-[50vh] border rounded-xl py-10 px-5 shadow-lg transition ease-in-out delay-150 hover:scale-105 hover:shadow-2xl">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="font-bold text-3xl text-center">To Do App</h1>
+        </div>
       </div>
     </div>
   );
