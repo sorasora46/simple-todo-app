eeb892f13734df744907491b1c383d945d0f5269
sora-k46 <sorrawit456789@gmail.com>
2022-12-22 00:42:56 +0700

    [+] add input bar and button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,14 @@ function App() {
     <div className="h-[100vh] flex justify-center items-center">
       <div className="w-[50%] h-[50vh] border rounded-xl py-10 px-5 shadow-lg transition ease-in-out delay-150 hover:scale-105 hover:shadow-2xl">
         <div className="flex flex-col justify-center items-center">
-          <h1 className="font-bold text-3xl text-center">To Do App</h1>
+          <h1 className="font-bold text-2xl text-center">To Do App</h1>
+          <div>
+            <form>
+              <input type="text" placeholder="Enter a task here" />
+              <button>SAVE</button>
+            </form>
+            <button>GET TASKS</button>
+          </div>
         </div>
       </div>
     </div>
