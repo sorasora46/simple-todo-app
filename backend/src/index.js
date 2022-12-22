875bd5063b150ade3ecca2c94a858b9f3a0201c5
sora-k46 <sorrawit456789@gmail.com>
2022-12-22 14:54:51 +0700

    [-] extract database logic to another file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,7 @@
 import express from "express"
-import { MongoClient } from "mongodb"
 
 const app = express()
 const PORT = 8000
-
-const mongoClient = new MongoClient("mongodb://localhost:27017/todo_list")
-mongoClient.connect((err, res) => {
-  if (err) throw err
-  console.log("connected to database")
-  res.close()
-})
 
 app.get("/", (req, res) => {
   res.send("hi")
