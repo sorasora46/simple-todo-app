912b5dae1e14c043eea808efe61c7aa71e8e0e97
sora-k46 <sorrawit456789@gmail.com>
2022-12-22 15:39:59 +0700

    [+] implement get todo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,27 @@
 import express from "express";
-import { addTodo } from "./mongoFunction.js";
+import { addTodo, getTodos } from "./mongoFunction.js";
 
 const app = express();
 const PORT = 8000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.post("/add", async (req, res) => {
+app.get("/get", (_, res) => {
+  getTodos()
+    .then((result) => {
+      res.status(200).json({ todo_list: result });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({ err: err });
+    });
+});
+
+app.post("/add", (req, res) => {
   addTodo(req.body.todo)
     .then((result) => {
       console.log("Added");
-      res.status(200).json({added: result.acknowledged})
+      res.status(200).json({ added: result.acknowledged });
     })
     .catch((err) => {
       console.log(err);
