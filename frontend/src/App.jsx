13e976415a758fbc6a388414658ce4d7d7ee8673
sora-k46 <sorrawit456789@gmail.com>
2022-12-22 13:29:51 +0700

    [+] add table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="h-[100vh] flex justify-center items-center">
-      <div className="w-[50%] h-[50vh] border rounded-xl py-10 px-5 shadow-lg transition ease-in-out delay-150 hover:scale-105 hover:shadow-2xl">
+      <div className="w-[50%] h-[50vh] border rounded-xl py-10 px-10 shadow-lg transition ease-in-out delay-150 hover:scale-105 hover:shadow-2xl">
         <div className="flex flex-col justify-center items-center">
           <h1 className="font-bold text-2xl text-center">To Do App</h1>
           <div className="w-[100%] flex justify-center gap-8 my-6">
@@ -36,8 +36,30 @@ function App() {
                 SAVE
               </button>
             </form>
-            <button className="rounded text-white bg-yellow-500 px-6 py-2 shadow-md shadow-yellow-500/50">GET TASKS</button>
+            <button className="rounded text-white bg-yellow-500 px-6 py-2 shadow-md shadow-yellow-500/50">
+              GET TASKS
+            </button>
           </div>
+          <table className="w-[100%]">
+            <tr>
+              <th>No.</th>
+              <th>Todo Item</th>
+              <th>Status</th>
+              <th>Actions</th>
+            </tr>
+            <tr>
+              <td>test</td>
+              <td>test</td>
+              <td>test</td>
+              <td>test</td>
+            </tr>
+            <tr>
+              <td>test</td>
+              <td>test</td>
+              <td>test</td>
+              <td>test</td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
