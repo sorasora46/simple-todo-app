e3da89385ab3c43fc976c872dbf71f82032a5b72
sora-k46 <sorrawit456789@gmail.com>
2022-12-22 14:37:57 +0700

    [+] connect to database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,15 @@
 import express from "express"
+import { MongoClient } from "mongodb"
+
 const app = express()
 const PORT = 8000
+
+const mongoClient = new MongoClient("mongodb://localhost:27017/todo_list")
+mongoClient.connect((err, res) => {
+  if (err) throw err
+  console.log("connected to database")
+  res.close()
+})
 
 app.get("/", (req, res) => {
   res.send("hi")
